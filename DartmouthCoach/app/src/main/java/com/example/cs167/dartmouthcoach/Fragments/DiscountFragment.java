d4cdada19cac483cb00c38303814f0501d9269a4
Xinbei <xinbei_fu@outlook.com>
2017-05-18 00:45:14 -0400

    user fragment implemented

--- DartmouthCoach/app/src/main/java/com/example/cs167/dartmouthcoach/Fragments/DiscountFragment.java
+++ DartmouthCoach/app/src/main/java/com/example/cs167/dartmouthcoach/Fragments/DiscountFragment.java
@@ -2,13 +2,16 @@ package com.example.cs167.dartmouthcoach.Fragments;
 
 import android.app.DialogFragment;
 import android.app.Fragment;
+import android.app.FragmentManager;
 import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.AdapterView;
+import android.widget.ImageView;
 import android.widget.ListView;
 
+import com.example.cs167.dartmouthcoach.MyDialogFragment;
 import com.example.cs167.dartmouthcoach.R;
 
 import java.util.ArrayList;
@@ -19,10 +22,44 @@ import java.util.ArrayList;
 
 public class DiscountFragment extends Fragment{
 
+    private ImageView boston_sdrt;
+    private ImageView ten_ride;
+    private ImageView children_discount;
+
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
         View rootView = inflater.inflate(R.layout.fragment_discount, container, false);
 
+        boston_sdrt = (ImageView) rootView.findViewById(R.id.sameday);
+        ten_ride = (ImageView) rootView.findViewById(R.id.commuter);
+        children_discount = (ImageView) rootView.findViewById(R.id.children);
+
+        boston_sdrt.setOnClickListener(new View.OnClickListener(){
+            public void onClick(View v){
+                MyDialogFragment dialog = MyDialogFragment.newInstance(1, "Same-Day Round-Trip",
+                        getActivity().getResources().getString(R.string.discount_sdrt_content));
+                dialog.show(getFragmentManager(), "dialog");
+            }
+        });
+
+        ten_ride.setOnClickListener(new View.OnClickListener(){
+            public void onClick(View v){
+                MyDialogFragment dialog = MyDialogFragment.newInstance(2, "10 Ride Commuter Pass",
+                        getActivity().getResources().getString(R.string.discount_commuter_content));
+                dialog.show(getFragmentManager(), "dialog");
+            }
+        });
+
+
+        children_discount.setOnClickListener(new View.OnClickListener(){
+            public void onClick(View v){
+                MyDialogFragment dialog = MyDialogFragment.newInstance(3, "Children's Deals & Discounts",
+                        getActivity().getResources().getString(R.string.discount_children));
+                dialog.show(getFragmentManager(), "dialog");
+            }
+        });
+
         return rootView;
     }
+
 }
--- DartmouthCoach/app/src/main/java/com/example/cs167/dartmouthcoach/UserProfile.java
+++ DartmouthCoach/app/src/main/java/com/example/cs167/dartmouthcoach/UserProfile.java
@@ -1,10 +1,23 @@
 package com.example.cs167.dartmouthcoach;
 
+import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.view.View;
 
 /**
  * Created by xinbeifu on 5/17/17.
  */
 
 public class UserProfile extends AppCompatActivity {
+
+    @Override
+    public void onCreate(Bundle savedInstanceState){
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_user_profile);
+
+    }
+
+    public void onSaveClicked(View view){
+        finish();
+    }
 }
